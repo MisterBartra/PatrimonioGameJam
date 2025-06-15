6a2f593af7418042689212467050fd963d232e3a
MisterBartra <jedubartra21@gmail.com>
2025-06-15 15:08:17 -0500

    SalirButton

--- Assets/Scripts/CreditsToggle.cs
+++ Assets/Scripts/CreditsToggle.cs
@@ -7,7 +7,12 @@ public class CreditsToggle : MonoBehaviour
 
     public void ToggleCredits()
     {
-        creditsPanel.SetActive(!creditsPanel.activeSelf);
-        transform.GetComponentInChildren<Text>().text = (!creditsPanel.activeSelf) ? "Créditos" : "Cerrar";
+        bool isPanelActive = creditsPanel.activeSelf; // Guarda estado antes de cambiarlo
+        creditsPanel.SetActive(!isPanelActive); // Alterna visibilidad del panel
+        transform.GetComponentInChildren<Text>().text = (!isPanelActive) ? "Cerrar" : "Créditos";
+    }
+    public void ToggleExit()
+    {
+        Application.Quit();
     }
 }
